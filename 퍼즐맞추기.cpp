70466d97bb4fb7eacd47aff7f95e372e30f97070
kimyoonha <kimyh9911@naver.com>
2020-09-27 16:55:26 +0900

    퍼즐 맞추기_수정본

--- 퍼즐맞추기.cpp
+++ 퍼즐맞추기.cpp
@@ -1,20 +1,27 @@
 ﻿#include <bangtal.h>
 #include <cstdlib>
-#include <ctime>
+#include <time.h>
 #include <string>
 #include <iostream>
 using namespace bangtal;
-
 class Rect {
 public:
-    int centerX, centerY, width, height;
+    int x, y;
 
-    Rect(int cx, int cy, int w, int h) : centerX(cx), centerY(cy), width(w), height(h) {}; //생성자 함수에 의해 초기화.
-
-    bool checkin(int x, int y) { //x, y가 cx, cy에 들어가는가
-        return (x > centerX - width) && (x < centerX + width) && (y > centerY - height) && (y < centerY + height);
-    }
+    Rect(int cx, int cy) : x(cx), y(cy) {}; //생성자 함수에 의해 초기화.
 };
+
+void swap(ObjectPtr a, ObjectPtr b, ScenePtr scene, int coordinates[]) {
+    int x = coordinates[0];
+    int y = coordinates[1];
+    int empty_x = coordinates[2];
+    int empty_y = coordinates[3];
+    a->locate(scene, x, y);
+    b->locate(scene, empty_x, empty_y);
+    a->show();
+    b->show();
+
+}
 
 int main()
 {
@@ -25,12 +32,15 @@ int main()
     auto scene = Scene::create("퍼즐맞추기", "Images/version1/full.png");
     auto start = Object::create("Images/start.png", scene, 640, 360, true);
     auto change = Object::create("Images/change.png", scene, 10, 10, true);
- 
+    auto timer = Timer::create(0.1f);
+
     const int nums_of_pictures = 9;
-    const int WIDTH = 150;
-    const int HEIGHT = 100;
     int version = 1;
     bool proceeding = false;
+    time_t startTime, endTime;
+    time_t maxTime = 1 << 30 - 1;
+    int times = 1 << 30;
+
 
     std::string routes_one[nums_of_pictures + 1] = {
     "",
@@ -63,18 +73,17 @@ int main()
         routes[i] = routes_one[i];
     }
 
-
     Rect rect[nums_of_pictures + 1] = {
-         {0, 0, 0, 0},
-         {340, 560, WIDTH, HEIGHT},
-         {640, 560, WIDTH, HEIGHT},
-         {940, 560, WIDTH, HEIGHT},
-       {340, 360, WIDTH, HEIGHT},
-       {640, 360, WIDTH, HEIGHT},
-       {940, 360, WIDTH, HEIGHT},
-         {340, 160, WIDTH, HEIGHT},
-         {640, 160, WIDTH, HEIGHT},
-         {940, 160, WIDTH, HEIGHT},
+         {0, 0 },
+         {190, 460},
+         {490, 460},
+         {790, 460},
+       {190, 260},
+       {490, 260},
+       {790, 260},
+         {190, 60},
+         {490, 60},
+         {790, 60},
     };
 
     ObjectPtr image[3][3];
@@ -82,34 +91,83 @@ int main()
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) which[i][j] = -1;
     }
+
     for (int i = 1; i <= nums_of_pictures; ++i) {
         int x = (i - 1) / 3;
         int y = (i - 1) % 3;
         image[x][y] = Object::create(routes[i], scene);
         image[x][y]->hide();
     }
+
     start->setOnMouseCallback([&](ObjectPtr object, int m, int n, MouseAction action) -> bool {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) which[i][j] = -1;
         }
         for (int i = 1; i <= nums_of_pictures; ++i) {
-            int num, x, y; // 들어갈 위치
-            do {
-                num = std::rand() % 9;
-                x = num / 3;
-                y = num % 3;
-                if (which[x][y] == -1) break;
-            } while (true);
-            which[x][y] = i;
-            if (i == 9) {
-                which[x][y] = 0;
-            }
+            int num = i - 1, x, y;
+            x = num / 3;
+            y = num % 3;
+            which[x][y] = i != 9 ? i : 0;
+
             image[x][y]->setImage(routes[i]);
-            image[x][y]->locate(scene, rect[num + 1].centerX - rect[num + 1].width, rect[num + 1].centerY - rect[num + 1].height);
+            image[x][y]->locate(scene, rect[i].x, rect[i].y);
             image[x][y]->show();
         }
+        times = 50;
+
+        timer->setOnTimerCallback([&](TimerPtr ptr)->bool {
+            times -= 1;
+            if (times == 0) return true;
+
+            int x = rand() % 3;
+            int y = rand() % 3;
+
+            int empty_x = -1;
+            int empty_y = -1;
+
+            for (int j = -1; j <= 1; j += 2) {
+                if (x + j >= 0 && x + j <= 2) {
+                    if (which[x + j][y] == 0) {
+                        empty_x = x + j;
+                        empty_y = y;
+                        break;
+                    }
+                }
+                if (y + j >= 0 && y + j <= 2) {
+                    if (which[x][y + j] == 0) {
+                        empty_x = x;
+                        empty_y = y + j;
+                        break;
+                    }
+                }
+            }
+
+            if (empty_x != -1) {
+
+                int empty = empty_x * 3 + empty_y + 1;
+                int i = x * 3 + y + 1;
+
+                int coordinates[4] = { rect[i].x, rect[i].y, rect[empty].x, rect[empty].y };
+
+                swap(image[empty_x][empty_y], image[x][y], scene, coordinates);
+
+                ObjectPtr temp = image[empty_x][empty_y];
+                image[empty_x][empty_y] = image[x][y];
+                image[x][y] = temp;
+                which[empty_x][empty_y] = which[x][y];
+                which[x][y] = 0;
+            }
+
+            timer->set(0.1f);
+            timer->start();
+
+            return true;
+        });
+
+        timer->start();
         start->hide();
         proceeding = true;
+        startTime = time(NULL);
         return true;
 
     });
@@ -144,14 +202,14 @@ int main()
                             if (empty_x == -1 || (empty_x == m && empty_y == n)) break;
                             int empty = empty_x * 3 + empty_y + 1;
                             int i = m * 3 + n + 1;
-                            image[empty_x][empty_y]->locate(scene, rect[i].centerX - rect[i].width, rect[i].centerY - rect[i].height);
-                            image[m][n]->locate(scene, rect[empty].centerX - rect[empty].width, rect[empty].centerY - rect[empty].height);
-                            image[empty_x][empty_y]->show();
-                            image[m][n]->show();
+
+                            int coordinates[4] = { rect[i].x, rect[i].y, rect[empty].x, rect[empty].y };
+
+                            swap(image[empty_x][empty_y], image[m][n], scene, coordinates);
+
                             ObjectPtr temp = image[empty_x][empty_y];
                             image[empty_x][empty_y] = image[m][n];
                             image[m][n] = temp;
-
                             which[empty_x][empty_y] = which[m][n];
                             which[m][n] = 0;
                             checked = true;
@@ -174,15 +232,16 @@ int main()
                 }
 
                 if (count == nums_of_pictures - 1) {
-                    showMessage("성공, 다 맞췄다!!!");
+                    endTime = time(NULL);
+                    if (maxTime > difftime(endTime, startTime)) maxTime = difftime(endTime, startTime);
+                    std::string buf = std::to_string(maxTime) + "초가 최고기록입니다!";
+                    showMessage(buf.c_str());
                     for (int i = 0; i < 3; i++) {
                         for (int j = 0; j < 3; j++) image[i][j]->hide();
                     }
                     start->show();
                     proceeding = false;
                 }
-
-
 
                 return true;
             });
